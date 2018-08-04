e1970fae0f69d3a82e8027db5e41677a9064631b
Santiago Perez Perret <perezperret@gmail.com>
2018-08-04 18:02:15 -0400

    reorder home highlight

--- views/Home.jsx
+++ views/Home.jsx
@@ -7,17 +7,17 @@ import Menu from '../components/Menu'
 
 const Home = () => (
   <div>
-    <div className="constrained container display">
-      <div className="text-tight text-right">
-        <span className="display-1">santiago</span>{' '}
-        <span className="display-1 text-secondary">perez</span>{' '}
-        <span className="display-1 text-primary">perret</span>{' '}
-      </div>
-    </div>
-
     <div className="bg-white mb-8">
       <div className="constrained container display">
         <p>{ABOUT}</p>
+      </div>
+    </div>
+
+    <div className="constrained container display justify-right">
+      <div className="text-tight text-right constrained-xs">
+        <span className="display-1">santiago</span>{' '}
+        <span className="display-1 text-secondary">perez</span>{' '}
+        <span className="display-1 text-primary">perret</span>{' '}
       </div>
     </div>
   </div>
