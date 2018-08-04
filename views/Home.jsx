d9999dbf04c7221a5214c32e895a7204aa68dfe2
Santiago Perez Perret <perezperret@gmail.com>
2018-08-04 17:51:37 -0400

    add home highlight

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import { ABOUT } from '../content/Home'
 
 import Menu from '../components/Menu'
 
 const Home = () => (
   <div>
-    <div className="bg-white">
-      <div className="constrained container display">
-        <p>{ABOUT}</p>
+    <div className="constrained container display">
+      <div className="text-tight text-right">
+        <span className="display-1">santiago</span>{' '}
+        <span className="display-1 text-secondary">perez</span>{' '}
+        <span className="display-1 text-primary">perret</span>{' '}
       </div>
     </div>
 
-    <div className="constrained container">
-      <Menu />
+    <div className="bg-white mb-8">
+      <div className="constrained container display">
+        <p>{ABOUT}</p>
+      </div>
     </div>
   </div>
 )
