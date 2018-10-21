02a896ad09b335079c3f47897dcb0a62de0ded3c
Santiago Perez Perret <perezperret@gmail.com>
2018-10-21 18:45:57 -0400

    Add about sections (Life and Work)

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,30 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { ABOUT } from '../content/Home'
-
-import Menu from '../components/Menu'
+import Jumbotron from '../components/Home/Jumbotron'
 
 const Home = () => (
-  <div className="container bg-white fold-height flex vertical align-end justify-center">
-    <div className="flex vertical align-end pb-3-1">
-      <div className="display-3 text-right text-muted">My name is</div>
-      <div className="display-1 text-right text-underline-primary">Santiago Perez Perret</div>
-    </div>
-
-    <div className="flex vertical align-end pb-3-1">
-      <div className="display-3 text-right text-muted">I work as a</div>
-      <div className="display-1 text-right text-underline-primary">Remote Web Developer</div>
-    </div>
+  <div>
+    <Jumbotron />
   </div>
 )
 
 export default Home
-
-// <div className="container display justify-right">
-//   <div className="text-tight text-right constrained-xs">
-//     <span className="display-1">santiago</span>{' '}
-//     <span className="display-1 text-secondary">perez</span>{' '}
-//     <span className="display-1 text-primary">perret</span>{' '}
-//   </div>
-// </div>
---
+++ views/Work.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { WORK } from '../content/Home'
+
+const Work = () => (
+  <div className="container">
+    <h1 className="my-2-1">My work</h1>
+    <p className="mb-2-1">{WORK}</p>
+  </div>
+)
+
+export default Work
