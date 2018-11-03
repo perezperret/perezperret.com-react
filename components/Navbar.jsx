4916e99685388a2cc2cf748436cb9e253a90321b
Santiago Perez Perret <perezperret@gmail.com>
2018-11-03 12:43:44 -0400

    Remove Navbar Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,49 +1,13 @@
 import React from 'react'
 
 import Logo from './Navbar/Logo'
-import Toggler from './Navbar/Toggler'
-import Tray from './Navbar/Tray'
 
-class Navbar extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { isOpen: false, alreadyToggled: false }
-
-    this.handleToggleTray = this.handleToggleTray.bind(this)
-    this.getNavbarClass = this.getNavbarClass.bind(this)
-  }
-
-  handleToggleTray() {
-    this.setState({
-      isOpen: !this.state.isOpen,
-      alreadyToggled: true
-    })
-  }
-
-  getNavbarClass() {
-    if (this.state.isOpen) return 'navbar--open'
-    if (!this.state.isOpen && this.state.alreadyToggled) return 'navbar--closed'
-    return ''
-  }
-
-  render() {
-    const { isOpen, alreadyToggled } = this.state
-
-    return (
-      <nav className="navbar">
-          <div className="navbar_menu">
-            <Logo onClick={() => {isOpen && this.handleToggleTray()}} />
-            <Toggler isOpen={isOpen} alreadyToggled={alreadyToggled} onClick={this.handleToggleTray} />
-          </div>
-
-          {
-            isOpen
-            ? <Tray isOpen={isOpen} alreadyToggled={alreadyToggled} onClick={this.handleToggleTray} />
-            : null
-          }
-      </nav>
-    )
-  }
-}
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="navbar_menu">
+      <Logo />
+    </div>
+  </nav>
+)
 
 export default Navbar
